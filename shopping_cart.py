7cb769bd5f7f68daa6d4a2d17281cd2a6df0d3ec
megc1 <mac492@georgetown.edu>
2019-02-03 13:07:44 -0500

    Separate info input and output processes

--- shopping_cart.py
+++ shopping_cart.py
@@ -25,17 +25,24 @@ products = [
 ] 
 
 total_price = 0
+selected_ids = []
 
 while True: 
     selected_id = input("Please input a product identifier. Enter \"DONE\" if finished. ")
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
 
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
 print("TOTAL PRICE = " + str(total_price))
